f941c05c24cacac384f3c7c23f4c44411eb777a1
Alexander Paniman <alexpaniman@gmail.com>
2023-05-08 05:40:17 +0300

    refactor: move math::rectangle to math folder

--- include/gl/math/rect.h
+++ include/gl/math/rect.h
@@ -1,17 +1,25 @@
 #pragma once
 
+#include "general.h"
 #include "math-utils.h"
 #include "vec.h"
+
+#include <cassert>
 
 namespace math {
 
     struct rectangle {
         math::vec2 x0, x1; // Top left point and bottom right point
 
-        explicit rectangle(math::vec2 top_left, math::vec2 bottom_right)
-            : x0(top_left), x1(bottom_right) {}
+        constexpr explicit rectangle(math::vec2 top_left, math::vec2 bottom_right)
+            : x0(top_left), x1(bottom_right) {
 
-        rectangle& shrink(math::vec2 delta) {
+
+            assert(x0.x() < x1.x() && "x0 should be at the left");
+            assert(x0.y() > x1.y() && "x0 should be at the top");
+        }
+
+        constexpr rectangle& shrink(math::vec2 delta) {
             x0 += delta;
             x1 -= delta;
 
@@ -31,6 +39,19 @@ namespace math {
         bool contains(math::vec2 position) {
             return math::in_range(position.x(), x0.x(), x1.x())
                 && math::in_range(position.y(), x0.y(), x1.y());
+        }
+
+        float height() { return x0.y() - x1.y(); }
+        float width()  { return x1.x() - x0.x(); }
+    };
+
+    template<>
+    struct zeroable<math::rectangle> {
+        static constexpr math::rectangle zero() {
+            return math::rectangle {
+                { 0.0f, 0.0f },
+                { 0.0f, 0.0f }
+            };
         }
     };
 
